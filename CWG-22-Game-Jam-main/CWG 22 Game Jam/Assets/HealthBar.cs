f54a04d49c609a23a4e06c8d83d791ec71b8b5f7
hailey-tran-9 <101952089+hailey-tran-9@users.noreply.github.com>
2022-03-20 02:23:36 -0700

    add dream gauge

---
+++ CWG-22-Game-Jam-main/CWG 22 Game Jam/Assets/HealthBar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void ResetHp() {
+        slider.maxValue = 100;
+        slider.value = 100;
+    }
+    
+    public void SetHp(int hp) {
+        slider.value = hp;
+    }
+}
