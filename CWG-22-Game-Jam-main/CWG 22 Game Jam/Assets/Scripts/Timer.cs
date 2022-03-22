19e0d0a3c3ec5bb5f6f5d85c84f4912858228744
Sara Viray <74330486+snviray@users.noreply.github.com>
2022-03-21 19:23:05 -0700

    fixed the wave stuff

--- CWG-22-Game-Jam-main/CWG 22 Game Jam/Assets/Scripts/Timer.cs
+++ CWG-22-Game-Jam-main/CWG 22 Game Jam/Assets/Scripts/Timer.cs
@@ -10,8 +10,8 @@ public class Timer : MonoBehaviour
     public float IncBy = 1;
     bool timerIsRunning = true;
     public Text timeText;
-    public GameObject NightmareSpawner;
-    // public nightmareScrip;
+    public GameObject nightmareSpawner;
+
 
     void Update()
     {
@@ -33,10 +33,39 @@ public class Timer : MonoBehaviour
         float minutes = Mathf.FloorToInt(currTime / 60); 
         float seconds = Mathf.FloorToInt(currTime % 60);
         timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        if (seconds == 0) {
+            if (minutes == 0) {
+                return;
+            }
+            if (minutes == 1) {
+                Checkpoint1();
+            } else if (minutes == 2) {
+                Checkpoint2();
+            } else if (minutes == 3) {
+                Checkpoint3();
+            }
+        }
     }
 
     //setting up checkpoints
     void Checkpoint1() {
-        // DreamSpawner nightmareScript = NightmareSpawner.GetComponent<DreamSpawner>;
+        Debug.Log("passed checkpoint 1");
+        DreamSpawner nightmareScript = nightmareSpawner.GetComponent<DreamSpawner>();
+        nightmareScript.currWaveSize = 10;
+        nightmareScript.spawnDelay =  1f;
+    }
+
+    void Checkpoint2() {
+        Debug.Log("passed checkpoint 2");
+        DreamSpawner nightmareScript = nightmareSpawner.GetComponent<DreamSpawner>();
+        nightmareScript.currWaveSize = 20;
+        nightmareScript.spawnDelay =  0.75f;
+    }
+
+    void Checkpoint3() {
+        Debug.Log("passed checkpoint 3");
+        DreamSpawner nightmareScript = nightmareSpawner.GetComponent<DreamSpawner>();
+        nightmareScript.currWaveSize = 40;
+        nightmareScript.spawnDelay =  0.5f;
     }
 }
