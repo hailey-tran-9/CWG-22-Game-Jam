39af9544371ef01dd222f886e8cd1957197c1713
hailey-tran-9 <101952089+hailey-tran-9@users.noreply.github.com>
2022-03-22 00:33:58 -0700

    add object sprites

--- CWG-22-Game-Jam-main/CWG 22 Game Jam/Assets/Scripts/Object.cs
+++ CWG-22-Game-Jam-main/CWG 22 Game Jam/Assets/Scripts/Object.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class Object : MonoBehaviour
 {
     public string type;
+    public Sprite buffSprite;
+    public Sprite debuffSprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        string objName = GetComponent<SpriteRenderer>().name;
-        Debug.Log(objName);
-        if (objName == "lego") {
+        float typeIndicator = Random.Range(0f, 10f);
+        if (typeIndicator <= 5f) {
             type = "debuff";
-        } else if (objName == "soda") {
+            GetComponent<SpriteRenderer>().sprite = debuffSprite;
+        } else {
             type = "buff";
+            GetComponent<SpriteRenderer>().sprite = buffSprite;
         }
+        Debug.Log(type + " spawned!");
     }
 }
